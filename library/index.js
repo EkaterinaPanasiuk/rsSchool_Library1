8b7cce9a3b67f5aabfe56b21940b3a206c7fa732
Ekaterina Panasiuk <ekaterinapanasiuk29@gmail.com>
2023-08-11 16:57:30 +0300

    add open-animation on burger-menu

--- library/index.js
+++ library/index.js
@@ -1,10 +1,28 @@
-const burger = document.getElementById("burger");
 document.addEventListener("click", (e) => {
-  if (e.target === burger) {
-    burger.classList.add("open");
-    console.log(e.target);
+  console.log(e.target);
+
+  if (
+    //open/close by burger-menu-icon
+    e.target.classList.contains("header-burger") ||
+    //close by click outsidethe menu
+    !(
+      e.target.classList.contains("open__menu")
+      /* ||
+      e.target.classList.contains("header__link") */
+    )
+  ) {
+    closeBurger();
   }
 });
+function closeBurger() {
+  const burger = document.querySelector(".header-burger");
+  const menu = document.querySelector(".header__menu");
+  const nav = document.querySelector(".header__nav");
+  burger.classList.toggle("close");
+  burger.classList.toggle("open");
+  menu.classList.toggle("open__menu");
+  nav.classList.toggle("animate-open");
+}
 
 console.log(
   " Общая оценка 100 баллов:\nВёрстка валидная +10 \n Вёрстка семантическая +16 \nВёрстка валидная +10 \n Вёрстка соответствует макету +54 \n   ---------(блок <header> +8) \n    ---------секция About +6 \n    --------секция Favorites \n    -------секция CoffeShop \n    ---------секция Contacts \n    --------секция LibraryCard \n    --------блок <footer> +8 \n    Общие требования к верстке +20\n favicon есть в футере и в закладке страницы(head)"
